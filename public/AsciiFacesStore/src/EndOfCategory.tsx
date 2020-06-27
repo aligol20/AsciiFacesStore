02fda6b16ada08a3450e4d5766151cfd1827922e
Ali Kargar <aligol20@gmail.com>
2020-06-27 10:51:47 +0430

    EndOfCategory.tsx

---
+++ public/AsciiFacesStore/src/EndOfCategory.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const EndOfCategory = () => {
+  return (
+    <View style={{width: '100%', height: 40}}>
+      <Text>{'end of category'}</Text>
+    </View>
+  );
+};
+export default EndOfCategory;
