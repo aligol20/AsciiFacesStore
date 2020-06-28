a650ad87f1d88b6a8e95fb532d00167bf5d36d35
Ali Kargar <aligol20@gmail.com>
2020-06-28 08:36:50 +0430

    Generates random and unique array Ids for Ads.

--- public/AsciiFacesStore/src/functions/RandomAdsProducer.ts
+++ public/AsciiFacesStore/src/functions/RandomAdsProducer.ts
@@ -1,13 +1,27 @@
+/**
+ * Generates random and unique array Ids for Ads.
+ * Due to test features:
+ * Ads should be randomly selected, but a user must never see the same ad twice in a row.
+ * But server produces just 10 URL of images,
+ * Length of random number It does not matter, the important thing is the first digit of the random number.
+ * so for creating the unique number, we should check the first digit of every random number that has been created.
+ * @param number - number of ads id that should be produced
+ */
 const RandomAdsProducer = (number: number) => {
-  const adsId = Math.floor(Math.random() * 1000);
   let result: number[] = [];
-  while (result.length < number) {
-    const random = Math.floor(Math.random() * 100) + 10;
-    const rest = random % 10;
+  // Checks, if the number of generated ids is enough.
 
+  while (result.length < number) {
+    // Generates a random number between 10 and 100.
+    const random = Math.floor(Math.random() * 100) + 10;
+    // Generates the first digit of 'random'.
+    const rest = random % 10;
+    // Checks, if there is a random number in the 'result' :
     result.length > 0
-      ? !result.find((x) => x % 10 === rest) && result.push(random)
-      : result.push(random);
+      ? // Checks the first digit of recently generated number with the first digit of numbers that stored in the 'result'.
+        !result.find((x) => x % 10 === rest) && result.push(random)
+      : // If the 'result' is empty, it doesn't care and pushes the generated number without care.
+        result.push(random);
   }
   return result;
 };
