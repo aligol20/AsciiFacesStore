daa018d6bcb9b45a549dfa2d2a04823d409ea884
Ali Kargar <aligol20@gmail.com>
2020-06-28 08:36:24 +0430

    Returns suitable 'url' according to the OS that is runing the code

--- public/AsciiFacesStore/src/Url.ts
+++ public/AsciiFacesStore/src/Url.ts
@@ -1,5 +1,13 @@
+/**
+ * Returns suitable 'url' according to the OS that is runing the code
+ */
 import {Platform} from 'react-native';
 
+/**
+ * Important: if you use an android device running the program,
+ * you should set you computer Ip address,
+ * to let android device connecting to the server correctly.
+ */
 export const url =
   Platform.OS === 'android'
     ? 'http://192.168.1.2:3000'
