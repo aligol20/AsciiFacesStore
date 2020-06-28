48c5ce3cb10b6911f7873ceb0c1fe35d046bab81
Ali Kargar <aligol20@gmail.com>
2020-06-29 00:50:21 +0430

    Doc for types added

--- public/AsciiFacesStore/src/Types.ts
+++ public/AsciiFacesStore/src/Types.ts
@@ -2,10 +2,19 @@
  * For making the code more cleaner I used the following.
  */
 
+// every section that contains 20 products, has this type
 export interface listItem {
   title: string;
-  data: {id: string; size: number; price: number}[][];
+  data: item[][];
 }
+// every product has this type
+export interface item {
+  id: string;
+  size: number;
+  price: number;
+}
+// available items for sorting the products
+
 export enum sortTypes {
   id = 'id',
   price = 'price',
