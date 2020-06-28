afb6d0e6a2e67f1bf0c0ea7c46b3cededd51fb9c
Ali Kargar <aligol20@gmail.com>
2020-06-28 08:36:38 +0430

    Converts cents to dollars.

--- public/AsciiFacesStore/src/functions/FormatPrice.ts
+++ public/AsciiFacesStore/src/functions/FormatPrice.ts
@@ -1,5 +1,11 @@
+/**
+ * Converts cents to dollars.
+ * @param price
+ */
 const FormatPrice = (price: number) => {
+  //Divides to 100 for finding the value as dollars.
   let test = price / 100;
+  // With the help of js function, converts it to dollars.
   const dollars = test.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
