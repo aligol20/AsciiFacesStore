85d30525d141c5ef2396b503bdc32d1ad112479f
Ali Kargar <aligol20@gmail.com>
2020-06-28 08:35:47 +0430

    A component for rendering the ads.

--- public/AsciiFacesStore/src/RenderAds.tsx
+++ public/AsciiFacesStore/src/RenderAds.tsx
@@ -1,10 +1,17 @@
+/**
+ * A component for rendering the ads.
+ */
 import React from 'react';
-import {Image, Dimensions} from 'react-native';
+import {Image} from 'react-native';
 import {styles} from './Styles';
 import {url} from './Url';
 
-const {width} = Dimensions.get('screen');
-
+/**
+ *
+ * @param adsId -It's an id that comes from 'SectionList', is a value that has follow properties:
+ * 1- returns a unique image.
+ * 2- has been selected randomly
+ */
 const RenderAds = (adsId: string) => {
   return (
     <Image
