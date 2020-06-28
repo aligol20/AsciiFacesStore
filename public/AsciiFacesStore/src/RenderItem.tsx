2c803a4f49d0883c8cb24d3e34e11dfadb2a08c0
Ali Kargar <aligol20@gmail.com>
2020-06-28 08:35:56 +0430

    Component for rendering the 20 AsciiFaces as a grid.

--- public/AsciiFacesStore/src/RenderItem.tsx
+++ public/AsciiFacesStore/src/RenderItem.tsx
@@ -1,15 +1,30 @@
+/**
+ * Component for rendering the 20 AsciiFaces as a grid.
+ */
 import React from 'react';
-import {View, Text} from 'react-native';
-import FormatPrice from './functions/FormatPrice';
+import {SectionListRenderItemInfo, Text, View} from 'react-native';
 import FormatDate from './functions/FormatDate';
+import FormatPrice from './functions/FormatPrice';
 import {styles} from './Styles';
+import {listItem} from './Types';
+
+/**
+ * For rendering the every section before ads,
+ * we should use 'map'.
+ * As readMe file included in the test file, every product should be viewed with its real size,
+ * Due to 'FlatList' limitation we couldn't show a Grid with the freedom in height and number of columns in every row,
+ * So I used map for rendering the every 20 products,
+ * As you saw in the 'Home.tsx', our main list maintainer is handled with 'SectionList'. So it doesn't affect the performance.
+ *
+ * @param item - value comes from 'sectionList'
+ */
 
 const RenderItem = (item: any) => {
   return (
     <View style={styles.item_container}>
       {item &&
         item.item &&
-        item.item.map((x: any) => (
+        item.map((x: any) => (
           <View key={x.id} style={styles.item_organizer}>
             <Text style={[styles.face, {fontSize: x.size}]}>{x.face}</Text>
             <Text style={styles.price}>{FormatPrice(x.price)}</Text>
